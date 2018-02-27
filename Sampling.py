c793e3a22bd1b167f48ad8ef35c9aa9b6d3ec624
Yasmin Emad <yasminemad.y96@gmail.com>
2018-02-27 22:36:27 +0200

    Update Sampling.py

--- Sampling.py
+++ Sampling.py
@@ -1,36 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#play with th sampling rate and maximum frequency
 time_of_view        = 1.; # s.
 analog_time         = np.linspace (0, time_of_view, 10e5); # s.
-
+#linspace Returns num evenly spaced samples, calculated over the interval [start, stop].
+# 10e5 dtype 
 sampling_rate       = 20.; # Hz
 sampling_period     = 1. / sampling_rate; # s
 sample_number       = time_of_view / sampling_period;
 sampling_time       = np.linspace (0, time_of_view, sample_number);
 
-carrier_frequency   = 9.;
+max_frequency   = 9.;  # maximum frequency 
+
+## the ratio sampling rate to carrier frequency should be > 2 according to Nyquist Criterion
+
+#the function parameters
 amplitude           = 1;
 phase               = 0;
 
-quantizing_bits     = 4;
-quantizing_levels   = 2 ** quantizing_bits / 2;
-quantizing_step     = 1. / quantizing_levels;
 
+#frequency function 
 def analog_signal (time_point):
-    return amplitude * np.cos (2 * np.pi * carrier_frequency * time_point + phase);
+    return amplitude * np.cos (2 * np.pi * max_frequency * time_point + phase);
+
 sampling_signal     = analog_signal (sampling_time);
-quantizing_signal   = np.round (sampling_signal / quantizing_step) * quantizing_step;
 
 
 fig = plt.figure () 
 plt.plot (analog_time,   analog_signal (analog_time) );
-plt.show() # used to plot the sampling only 
-#plt.stem (sampling_time, sampling_signal); 
-plt.stem (sampling_time, quantizing_signal, linefmt='r-', markerfmt='rs', basefmt='r-');
-plt.title("Analog to digital signal conversion")
+plt.title(" Signal ")
 plt.xlabel("Time")
 plt.ylabel("Amplitude")
+plt.show ()
+
+plt.plot (analog_time,   analog_signal (analog_time) );
+plt.plot (sampling_time, sampling_signal, 'o')
+plt.show() # used to plot the sampling only 
 
 
-plt.show()
+
